00861bcd11f6d95e05983892e082c3bcc4fd698b
antara srivastava <118758547+antarades@users.noreply.github.com>
2025-06-23 14:21:00 +0530

    app.py

--- backend/app.py
+++ backend/app.py
@@ -81,6 +81,8 @@ def handle_new_patch(patch_data):
     emit('update_quilt', response_patch, broadcast=True)
 
 if __name__ == '__main__':
+    import os
     with app.app_context():
         db.create_all()
-    socketio.run(app, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
